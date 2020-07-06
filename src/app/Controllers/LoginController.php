786f67e2cb37b97f72bffff4f5e2eb318cf3b27b
Franck DAKIA <dakiafranck@gmail.com>
2020-07-06 03:40:49 +0200

    feat: add show login form method

--- src/app/Controllers/LoginController.php
+++ src/app/Controllers/LoginController.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace Bow\Niack\Controllers\Dashboard;
+namespace Bow\Niack\Controllers;
 
 use Bow\Http\Request;
 
 class LoginController extends \App\Controllers\Controller
 {
-    //
+    /**
+     * Show the login form
+     *
+     * @return void
+     */
+    public function showLogin()
+    {
+        return view('niack::login');
+    }
 }
